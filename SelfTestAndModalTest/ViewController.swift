12346a351527e5b6318c58766f54cbe1b262d880
showcove <showcove@gmail.com>
2020-06-11 12:07:10 +0900

    first commit

---
+++ SelfTestAndModalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfTestAndModalTest
+//
+//  Created by 김재한 on 2020/06/11.
+//  Copyright © 2020 jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
